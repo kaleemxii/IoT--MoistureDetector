cc7735fa55748fd36c0d6e3b4f35149f6a053a99
Kaleem Shaik <kashaik@microsoft.com>
2019-09-26 15:47:23 +0530

    final

---
+++ message.ino
@@ -0,0 +1,76 @@
+#include <ArduinoJson.h>
+#include <Wire.h>
+
+const int sensor_pin = A0;  /* Connect Soil moisture analog sensor pin to A0 of NodeMCU */
+
+void initSensor()
+{
+    
+}
+
+
+float readSoilMoisture()
+{
+    float moisture_percentage, moisture_value;
+
+    //moisture_percentage = ( 100.00 - ( (analogRead(sensor_pin)/1023.00) * 100.00 ) );
+    moisture_value= analogRead(sensor_pin);
+    moisture_value= moisture_value/10;
+    return moisture_value;
+}
+
+
+bool readMessage(int messageId, char *payload)
+{
+    bool temperatureAlert = false;
+    float temperature = 50.0;
+    float moisture_value = readSoilMoisture();
+    if (moisture_value > 90)
+    {
+       Serial.println("Detected Dryness. Turning on LED.");
+       digitalWrite(ledPin, LOW);
+       temperatureAlert = true;
+    }
+    else
+    {
+       digitalWrite(ledPin, HIGH);
+    }
+    
+    StaticJsonBuffer<MESSAGE_MAX_LEN> jsonBuffer;
+    JsonObject &root = jsonBuffer.createObject();
+    root["deviceId"] = DEVICE_ID;
+    root["messageId"] = messageId;
+    
+
+    
+    if (std::isnan(moisture_value))
+    {
+        root["moisture_value"] = NULL;
+    }
+    else
+    {
+        root["moisture_value"] = moisture_value;
+    }
+    root.printTo(payload, MESSAGE_MAX_LEN);
+    return temperatureAlert;
+}
+
+void parseTwinMessage(char *message)
+{
+    StaticJsonBuffer<MESSAGE_MAX_LEN> jsonBuffer;
+    JsonObject &root = jsonBuffer.parseObject(message);
+    if (!root.success())
+    {
+        Serial.printf("Parse %s failed.\r\n", message);
+        return;
+    }
+
+    if (root["desired"]["interval"].success())
+    {
+        interval = root["desired"]["interval"];
+    }
+    else if (root.containsKey("interval"))
+    {
+        interval = root["interval"];
+    }
+}
